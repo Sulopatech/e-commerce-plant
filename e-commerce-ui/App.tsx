2b606d6f20f005e5984006e07cfae2a5a8bb7feb
Mohantymonika <mohantymonika28@gmail.com>
2024-06-21 12:06:14 +0530

    Resolve Conflict.

--- e-commerce-ui/App.tsx
+++ e-commerce-ui/App.tsx
@@ -1,14 +1,17 @@
-import {Provider} from 'react-redux';
-import React, {useEffect} from 'react';
-import {components} from './src/components';
-import {persistor, store} from './src/store';
-import {enableScreens} from 'react-native-screens';
+import { Provider } from 'react-redux';
+import React, { useEffect } from 'react';
+import { components } from './src/components';
+import { persistor, store } from './src/store';
+import { enableScreens } from 'react-native-screens';
 import Orientation from 'react-native-orientation-locker';
-import {PersistGate} from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/integration/react';
 import StackNavigator from './src/navigation/StackNavigator';
-import {NavigationContainer} from '@react-navigation/native';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider, ApolloLink, Observable } from '@apollo/client';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import { API_URL } from './src/API/Environment';
 enableScreens();
 
 const App = () => {
@@ -16,17 +19,45 @@ const App = () => {
     Orientation.lockToPortrait();
   }, []);
 
+  const httpLink = new HttpLink({ uri: API_URL });
+
+  const authMiddleware = new ApolloLink((operation, forward) => {
+    return new Observable(observer => {
+      (async () => {
+        const token = await AsyncStorage.getItem("token");
+        operation.setContext({
+          headers: {
+            authorization: token ? `Bearer ${token}` : "",
+          },
+        });
+        const subscriber = {
+          next: observer.next.bind(observer),
+          error: observer.error.bind(observer),
+          complete: observer.complete.bind(observer),
+        };
+        forward(operation).subscribe(subscriber);
+      })();
+    });
+  });
+
+  const client = new ApolloClient({
+    link: ApolloLink.from([authMiddleware, httpLink]),
+    cache: new InMemoryCache(),
+  });
+
   return (
     <SafeAreaProvider>
-      <Provider store={store}>
-        <PersistGate loading={<components.Loader />} persistor={persistor}>
-          <NavigationContainer>
-            <StackNavigator />
-          </NavigationContainer>
-        </PersistGate>
-        <components.AppState />
-      </Provider>
-      <components.FlashMessage />
+      <ApolloProvider client={client}>
+        <Provider store={store}>
+          <PersistGate loading={<components.Loader />} persistor={persistor}>
+            <NavigationContainer>
+              <StackNavigator />
+            </NavigationContainer>
+          </PersistGate>
+          <components.AppState />
+        </Provider>
+        <components.FlashMessage />
+      </ApolloProvider>
     </SafeAreaProvider>
   );
 };
