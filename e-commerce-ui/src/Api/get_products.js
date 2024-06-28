00209a5502e7005e61ae8a2f61906123ec6883e9
Bholasankar nanda <bholasankarnanda592@gmail.com>
2024-06-28 11:08:50 +0530

    "product"

--- e-commerce-ui/src/Api/get_products.js
+++ e-commerce-ui/src/Api/get_products.js
@@ -1,17 +1,17 @@
 import { gql } from '@apollo/client';
 
 export const GET_ALL_PRODUCTS = gql`
-    query GetProducts($options: ProductListOptions) {
-        products(options: $options) {
-            items {
-                id
-                name
-                slug
-                featuredAsset {
-                    preview
-                }
-            }
-            totalItems
+  query GetProducts($options: ProductListOptions) {
+    products(options: $options) {
+      items {
+        id
+        name
+        slug
+        image: featuredAsset {
+          preview
         }
+      }
+      totalItems
     }
+  }
 `;
