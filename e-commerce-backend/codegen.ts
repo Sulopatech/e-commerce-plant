5fad67aa147cf5a7e497623e8944912d7076b4c0
rishikumar <rishikumar@sulopa.com>
2024-07-04 15:54:17 +0530

    Migration , env, Codegen

--- e-commerce-backend/codegen.ts
+++ e-commerce-backend/codegen.ts
@@ -15,6 +15,7 @@ const config: CodegenConfig = {
         './src/plugins/productreview/gql/generated.ts': { plugins: ['typescript'] },
         './src/plugins/deletecustomer/gql/generated.ts': { plugins: ['typescript'] },
         './src/plugins/extendedcollection/gql/generated.ts': { plugins: ['typescript'] },
+        './src/plugins/banners/gql/generated.ts': { plugins: ['typescript'] },
     },
 };
 
