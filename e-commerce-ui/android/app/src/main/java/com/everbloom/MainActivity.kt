8bfce63232fc3e432e9b16450601b3d8e028cccb
Swagat Suman <swagat@sulopa.com>
2024-08-14 14:31:56 +0530

    Plant mobile app setup

--- e-commerce-ui/android/app/src/main/java/com/everbloom/MainActivity.kt
+++ e-commerce-ui/android/app/src/main/java/com/everbloom/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.everbloom
+package com.sulopa.plant
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- e-commerce-ui/android/app/src/main/java/com/everbloom/MainApplication.kt
+++ e-commerce-ui/android/app/src/main/java/com/everbloom/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.everbloom
+package com.sulopa.plant
 
 import android.app.Application
 import com.facebook.react.PackageList
